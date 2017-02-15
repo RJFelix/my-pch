b4a2af41e9f92641c814153d51317b04ee3a348e
R F <a>
2017-02-15 00:18:04 -0800

    add some more items to the initial state

--- client/src/app/places/ducks/initialState.js
+++ client/src/app/places/ducks/initialState.js
@@ -51,6 +51,51 @@ const initialState = {
             visible: true,
             selected: false
 
+        },
+        { 
+            id: 3,
+            itemName: "Pebble Beach",
+            image: "https://media-cdn.tripadvisor.com/media/photo-s/00/1a/b4/e6/pebble-beach.jpg",
+            description: "Of course he's a happy little stone, cause we don't have any other kind. This is where you take out all your hostilities and frustrations. It's better than kicking the puppy dog around and all that so. You need the dark in order to show the light. Nature is so fantastic, enjoy it. Let it make you happy.",
+            tags: ["golfing"],
+            coords: // google.maps.LatLngLiteral
+            {
+                lat: 36.5635,
+                lng: -121.9306
+            },
+            visible: true,
+            selected: false
+
+        },
+        { 
+            id: 4,
+            itemName: "Canoe on the Rogue River",
+            image: "http://i.imgur.com/DfkXAC5.jpg",
+            description: "This is unplanned it really just happens. Just beat the devil out of it. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff. This is your world. The only thing worse than yellow snow is green snow.",
+            tags: ["boating"],
+            coords: // google.maps.LatLngLiteral
+            {
+                lat: 42.5069,
+                lng: -124.1411
+            },
+            visible: true,
+            selected: false
+
+        },
+        { 
+            id: 5,
+            itemName: "Cape Flattery",
+            image: "http://i.imgur.com/DkMcQVo.jpg",
+            description: "Work that paint. The man who does the best job is the one who is happy at his job. The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it.",
+            tags: ["nature"],
+            coords: // google.maps.LatLngLiteral
+            {
+                lat: 48.3863,
+                lng: -124.7074
+            },
+            visible: true,
+            selected: false
+
         }
     ],
     mapBounds: {
@@ -64,7 +109,9 @@ const initialState = {
         camping: true,
         surfing: false,
         hiking: false,
-        nature: false
+        nature: false,
+        golfing: false,
+        boating: false
     }
 };
 
